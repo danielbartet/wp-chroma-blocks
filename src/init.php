d6a0f50db203118d2cc4b207dac371ba471025fa
Daniel Bartet <dbartet@progroupx.com>
2024-03-28 00:48:05 -0300

    add config block

--- src/init.php
+++ src/init.php
@@ -11,6 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 function chroma_blocks_category( $categories, $post ) {
+	error_log('El filtro de categoría de bloques se está ejecutando.');
     return array_merge(
         $categories,
         array(
@@ -27,7 +28,7 @@ add_filter('block_categories_all', 'chroma_blocks_category', 10, 2);
 
 function chroma_blocks_assets() {
 	// Styles.
-	wp_register_style( 'cm-style', plugins_url( 'dist/blocks.style.build.css', dirname( __FILE__ )), null,  filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ));
+	wp_register_style( 'cm-style', __DIR__ . 'dist/blocks.style.build.css'), null,  filemtime( __DIR__  . 'dist/blocks.style.build.css' ));
 	wp_enqueue_style('cm-style');
 }
 add_action( 'enqueue_block_assets', 'chroma_blocks_assets', 100 );
@@ -37,20 +38,37 @@ function chroma_blocks_cgb_editor_assets() {
 	// Scripts.
 	wp_enqueue_script(
 		'chroma_blocks-cgb-block-js', // Handle.
-		plugins_url( '/dist/blocks.build.js', dirname( __FILE__, 2 ) ), // Block.build.js: We register the block here. Built with Webpack.
+		__DIR__ . '/dist/blocks.build.js', // Block.build.js: We register the block here. Built with Webpack.
 		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ), // Dependencies, defined above.
-		filemtime( plugin_dir_path( __DIR__ ) . '/dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
+		filemtime(  __DIR__  . '/dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
 		true // Enqueue the script in the footer.
 	);
 
 	wp_enqueue_style(
 		'chroma_blocks-cgb-block-editor-css', // Handle.
-		plugins_url( '/dist/blocks.editor.build.css', dirname( __FILE__, 2 ) ), // Block style CSS. NOTE WE ARE CHANGING THIS TO DEFAULT STYLE
+		__DIR__ . '/dist/blocks.editor.build.css', // Block style CSS. NOTE WE ARE CHANGING THIS TO DEFAULT STYLE
 		array( 'wp-edit-blocks' ), // Dependency to include the CSS after it.
-		filemtime( plugin_dir_path( __DIR__ ) . '/dist/blocks.editor.build.css' )
+		filemtime(  __DIR__  . '/dist/blocks.editor.build.css' )
 		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: filemtime — Gets file modification time.
 	);
 } // End function chroma_blocks_cgb_editor_assets().
 
 // Hook: Editor assets.
 add_action( 'enqueue_block_editor_assets', 'chroma_blocks_cgb_editor_assets' );
+
+function chroma_blocks_register_block_type() {
+    $blocks = [
+        '/blocks/chroma-quote',
+        '/blocks/chroma-button',
+        '/blocks/chroma-media',
+        '/blocks/slider-block',
+        '/blocks/quiz-block',
+        '/blocks/select-quiz',
+        '/blocks/product-card',
+    ];
+
+    foreach ( $blocks as $block ) {
+        register_block_type_from_metadata( __DIR__ . $block );
+    }
+}
+add_action( 'init', 'chroma_blocks_register_block_type' );
