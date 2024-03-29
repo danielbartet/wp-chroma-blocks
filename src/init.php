db78d1dca678427da2a74f0a07f5c4cc30dd07e5
Daniel Bartet <dbartet@progroupx.com>
2024-03-29 01:03:43 -0300

    Fix bug

--- src/init.php
+++ src/init.php
@@ -30,7 +30,7 @@ add_filter('block_categories_all', 'chroma_blocks_category', 10, 2);
 
 function chroma_blocks_assets() {
 	// Styles.
-	wp_register_style( 'cm-style', plugins_url( '../dist/blocks.style.build.css', __FILE__ ), null,  filemtime( __DIR__  . 'dist/blocks.style.build.css' ));
+	wp_register_style( 'cm-style', PLUGIN_DIR.'/dist/blocks.style.build.css', null,  filemtime( PLUGIN_DIR  . '/dist/blocks.style.build.css' ));
 	wp_enqueue_style('cm-style');
 }
 add_action( 'enqueue_block_assets', 'chroma_blocks_assets', 100 );
@@ -40,17 +40,17 @@ function chroma_blocks_cgb_editor_assets() {
 	// Scripts.
 	wp_enqueue_script(
 		'chroma_blocks-cgb-block-js', // Handle.
-		plugins_url( '../dist/blocks.build.js', __FILE__ ), // Block.build.js: We register the block here. Built with Webpack.
+		PLUGIN_DIR.'dist/blocks.build.js', // Block.build.js: We register the block here. Built with Webpack.
 		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ), // Dependencies, defined above.
-		filemtime( plugins_url( '../dist/blocks.build.js', __FILE__ ) ), // Version: filemtime — Gets file modification time.
+		filemtime( PLUGIN_DIR . 'dist/blocks.editor.build.css' ), // Version: filemtime — Gets file modification time.
 		true // Enqueue the script in the footer.
 	);
 
 	wp_enqueue_style(
 		'chroma_blocks-cgb-block-editor-css', // Handle.
-		plugins_url( '../dist/blocks.editor.build.css', __FILE__ ), // Block style CSS. NOTE WE ARE CHANGING THIS TO DEFAULT STYLE
+		PLUGIN_DIR. 'dist/blocks.editor.build.css', // Block style CSS. NOTE WE ARE CHANGING THIS TO DEFAULT STYLE
 		array( 'wp-edit-blocks' ), // Dependency to include the CSS after it.
-		filemtime(  plugins_url( '../dist/blocks.editor.build.css', __FILE__ ) )
+		filemtime(  PLUGIN_DIR . 'dist/blocks.editor.build.css' )
 		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: filemtime — Gets file modification time.
 	);
 } // End function chroma_blocks_cgb_editor_assets().
