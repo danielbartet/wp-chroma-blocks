954a0c97b86ca9bd6ee8d2aca403bec068e95d35
Daniel Bartet <dbartet@progroupx.com>
2024-03-29 18:58:58 -0300

    build

--- dist/blocks.build.asset.php
+++ dist/blocks.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks'), 'version' => '0fbed6c85ff17e9f62ad');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '41807166c55ee51628a2');
