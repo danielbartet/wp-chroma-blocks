4f9945074036aa2d48302080784b61f9d1121084
Daniel Bartet <dbartet@progroupx.com>
2024-03-29 22:11:39 -0300

    build

--- dist/blocks.build.asset.php
+++ dist/blocks.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '443ba90c9d8016a5f4fe');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6fd9fb985377a4301e8b');
--- src/blocks/chroma-media/media.js
+++ src/blocks/chroma-media/media.js
@@ -1,190 +1,100 @@
-/**
- * BLOCK: chroma-blocks
- *
- * Registering a basic block with Gutenberg.
- * Simple block, renders and saves the same content without any interactivity.
- */
-
-//  Import CSS.
+// Import CSS.
 import './style.scss';
 import './editor.scss';
 
 import { registerBlockType } from '@wordpress/blocks';
-const {
-  Editable,
-  MediaUpload,
-  AlignmentToolbar,
-  BlockControls,
-  BlockAlignmentToolbar,
-} = wp.blockEditor;
-const {
-  Button, Toolbar, Tooltip
-} = wp.components;
+import { MediaUpload, BlockControls, AlignmentToolbar } from '@wordpress/block-editor';
+import { Button, Toolbar, Tooltip } from '@wordpress/components';
 
-registerBlockType( 'chroma-blocks/media-upload', {
-	title: 'Chroma Upload',
-	icon: 'format-image',
-	category: 'Chroma',
-	attributes: {
-    imgURL: {
-      type: 'string',
-      source: 'attribute',
-      attribute: 'src',
-      selector: 'img',
+registerBlockType('chroma-blocks/media-upload', {
+    title: 'Chroma Upload',
+    icon: 'format-image',
+    category: 'chroma',
+    attributes: {
+        imgURL: {
+            type: 'string',
+            source: 'attribute',
+            attribute: 'src',
+            selector: 'img',
+        },
+        imgID: {
+            type: 'number',
+        },
+        imgAlt: {
+            type: 'string',
+            source: 'attribute',
+            attribute: 'alt',
+            selector: 'img',
+        },
+        caption: {
+            type: 'string',
+            source: 'html',
+            selector: '.figcaption_link',
+        },
+        captionLink: {
+            type: 'string',
+            source: 'attribute',
+            attribute: 'href',
+            selector: '.figcaption_link',
+        },
+        alignment: {
+            type: 'string',
+            default: '',
+        },
     },
-    imgID: {
-      type: 'number',
+    edit: (props) => {
+        const { attributes: { imgID, imgURL, imgAlt, caption, captionLink, alignment }, className, setAttributes } = props;
+
+        const onSelectImage = (img) => {
+            setAttributes({
+                imgID: img.id,
+                imgURL: img.url,
+                imgAlt: img.alt,
+            });
+        };
+
+        const onChangeAlignment = (newAlignment) => {
+            setAttributes({ alignment: newAlignment });
+        };
+
+        return (
+            <div className={className}>
+                <BlockControls>
+                    <AlignmentToolbar value={alignment} onChange={onChangeAlignment} />
+                </BlockControls>
+                <MediaUpload
+                    onSelect={onSelectImage}
+                    allowedTypes={['image']}
+                    value={imgID}
+                    render={({ open }) => (
+                        <Button onClick={open} className={!imgID ? 'button button-large' : 'image-button'}>
+                            {!imgID ? 'Upload Image' : <img src={imgURL} alt={imgAlt} />}
+                        </Button>
+                    )}
+                />
+                {caption && (
+                    <figcaption className="figcaption">
+                        <a href={captionLink} className="figcaption_link" target="_blank" rel="noopener noreferrer">
+                            {caption}
+                        </a>
+                    </figcaption>
+                )}
+            </div>
+        );
     },
-    imgAlt: {
-      type: 'string',
-      source: 'attribute',
-      attribute: 'alt',
-      selector: 'img',
+    save: ({ attributes }) => {
+        const { imgURL, imgAlt, caption, captionLink, alignment } = attributes;
+        return (
+            <div className={`wp-block-chroma-blocks-media-upload align${alignment}`}>
+                <img src={imgURL} alt={imgAlt} />
+                {caption && (
+                    <figcaption className="figcaption">
+                        <a href={captionLink} className="figcaption_link" target="_blank" rel="noopener noreferrer">
+                            {caption}
+                        </a>
+                    </figcaption>
+                )}
+            </div>
+        );
     },
-    caption: {
-      type: 'array',
-      source: 'children',
-      selector: '.figcaption_link'
-    },
-    captionLink: {
-      type: 'string',
-      source: 'attribute',
-      attribute: 'href',
-      selector: '.figcaption_link'
-    },
-    center: {
-      type: 'boolean',
-      default: false,
-    },
-    centerDark: {
-      type: 'boolean',
-      default: false,
-    },
-    contStyle: {
-      type: 'string',
-      source: 'attribute',
-      attribute: 'class',
-      selector: '.wp-block-chroma-blocks-media-upload',
-    },
-    alignment: {
-      type: 'string',
-      default: 'none'
-    }
-	},
-	edit: props => {
-    const { attributes: { imgID, imgURL, imgAlt, caption, captionLink, center, centerDark, contStyle, alignment }, className, setAttributes, isSelected } = props;
-    const onSelectImage = img => {
-        var captionLink = null
-        fetch(`/wp-json/wp/v2/media/${img.id}`)
-        .then(function(response) {
-          return response.json()
-        })
-        .then(function(myJson) {
-          if (typeof myJson.meta != 'undefined') {
-            captionLink = myJson.meta.chroma_caption_hyperlink
-            return captionLink
-          }
-        })
-        .then(function(captionLink) {
-          setAttributes({
-              imgID: img.id,
-              imgURL: img.url,
-              imgAlt: img.alt,
-              caption: img.caption,
-              captionLink: captionLink
-          })
-        })
-    }
-    const onRemoveImage = () => {
-      setAttributes({
-        imgID: null,
-        imgURL: null,
-        imgAlt: null,
-      });
-    }
-    const onChangeAlignment = (newAlignment) => {
-      props.setAttributes({
-        alignment: newAlignment === undefined ? 'none' : newAlignment
-      })
-    }
-    const getImgContainer = () => {
-      let imgCont = 'image-container',
-          imgDark = 'image-container--dark',
-          classList = []
-      if (center)
-        classList.push(imgCont)
-      else if(classList.indexOf(imgCont) != -1)
-        classList.remove(imgCont)
-      if (centerDark)
-        classList.push(imgCont, imgDark)
-      else if (classList.indexOf(imgDark) != -1)
-        classList.remove(imgCont, imgDark)
-      classList.push(alignment)
-      props.setAttributes({
-        contStyle: classList.join(' ')
-      })
-      return classList.join(' ')
-    }
-		return (
-      <div className={getImgContainer()}>
-        <BlockControls>
-          <AlignmentToolbar value={ alignment } onChange={ onChangeAlignment } />
-          <Toolbar>
-            <Tooltip text="Center">
-              <Button className="components-button" style={center ? {background: '#4e4e4e', color: '#fff'}:null} onClick={ () => setAttributes( { center: ! center } ) }>
-                Container
-              </Button>
-            </Tooltip>
-            <Tooltip text="Center Dark">
-              <Button className='components-button' style={centerDark ? {background: '#4e4e4e', color: '#fff'}:null} onClick={ () => setAttributes( { centerDark: ! centerDark } ) }>
-                Dark
-              </Button>
-            </Tooltip>
-          </Toolbar>
-        </BlockControls>
-      <figure className={caption ? 'entry-content_figure fig-wcaption' : 'entry-content_figure' }>
-        <MediaUpload
-          onSelect={ onSelectImage }
-          allowedTypes="image"
-          value={ imgID }
-          render={ ( { open } ) => (
-            <Button className={ imgID ? 'image-button' : 'button button-large' } onClick={ open }>
-              { ! imgID ? 'Add Image' : <img src={ imgURL } alt={ imgAlt } /> }
-            </Button>
-          ) }
-        />
-        { caption ? (
-          <figcaption className="figcaption">
-            <a href={ captionLink } className="figcaption_link" target="_blank" rel="noopener">
-              { caption }
-            </a>
-          </figcaption>
-        ) : null
-        }
-      </figure>
-      </div>
-		);
-	},
-	save: props => {
-    const { imgURL, imgAlt, caption, captionLink, center, centerDark, contStyle } = props.attributes;
-    const figCapClass = (caption.length > 0) ? 'entry-content_figure fig-wcaption' : 'entry-content_figure';
-    return (
-      <div className={contStyle}>
-        <figure className={figCapClass}>
-          <img
-            src={ imgURL }
-            alt={ imgAlt }
-          />
-          { caption.length > 0 ? (
-            <figcaption className="figcaption">
-              <a href={captionLink} className="figcaption_link" target="_blank" rel="noopener">
-                {caption}
-              </a>
-            </figcaption>
-          ) : null
-          }
-        </figure>
-      </div>
-  	)
-  }
-})
+});
