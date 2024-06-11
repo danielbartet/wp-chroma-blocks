7e8f5bb11a1cb6499e66b636597408675a86d531
Daniel Bartet <dbartet@progroupx.com>
2024-06-11 00:27:11 -0300

    fix border slider block

--- dist/blocks.build.asset.php
+++ dist/blocks.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7d792209cb5a58ddb7b6');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e0424f1158d4d3711ae0');
